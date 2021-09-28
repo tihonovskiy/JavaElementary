d5504e2723b889fb0aeee75722eaafb8d6cb4345
Kirill Tihonovskiy <tihonovskiy@gmail.com>
2021-09-28 22:17:22 +0300

    add gitignore file and HW1-Task1-ReplacementOfNumbers

---
+++ src/HW1/ReplacementOfNumbers.java
@@ -0,0 +1,17 @@
+package HW1;
+
+public class ReplacementOfNumbers {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++) {
+            if(i%3==0 && i%5==0) {
+                System.out.print("HelloWorld ");
+            } else if(i%3==0) {
+                System.out.print("Hello ");
+            } else if(i%5==0) {
+                System.out.print("World ");
+            } else{
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

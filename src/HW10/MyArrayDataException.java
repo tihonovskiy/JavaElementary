238dcdac29147440c58c65968504c4024e715efa
Kirill Tihonovskiy <tihonovskiy@gmail.com>
2021-11-04 18:46:18 +0200

    hw10 - exceptions

---
+++ src/HW10/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package HW10;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
